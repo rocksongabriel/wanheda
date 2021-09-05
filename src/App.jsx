286cc78dcd93125337c21a2e4b2c28ad71b69e24
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-09-05 21:22:55 +0000

    Fresh project install

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="text-gray-900 font-bold text-center text-3xl md:text-5xl lg:text-8xl py-4">Welcome to Wanheda</h1>
+      </div>
+    )
+  }
+}
+
+
+export default App;
